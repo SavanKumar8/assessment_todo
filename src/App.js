67d3f7e2147a7f14c54ba9d8c12a49d3285e7352
Savan Kumar <SavanKumar@eagle.org>
2020-01-20 15:31:00 +0530

    =Assessment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Todo =()=>{
+  const [state, setstate] = useState({toDoList: [
+  {taskName:'Washing', completed:false},
+  {taskName:'Cooking', completed:false}
+],
+errorMessage: ''});
+
+
+// const inputRef = React.createRef();// to clear input field 
+
+const addTaskListener =(task) =>{
+  if(task){
+    setstate(prevstate=>{
+      const data = [...prevstate.toDoList] //spread the toDoList array from state object.
+      data.push({taskName:`${task}`,completed:false})
+      // inputRef.value=''
+      document.getElementById('task').value = ''
+      return {...prevstate,toDoList:data, errorMessage: ''} //updating the toDoList with data array
+    })
+  }else{
+    setstate(prevstate=>{
+      return {...prevstate, errorMessage:'Please enter a task'} //updating the toDoList with data array
+    })
+  } 
 }
 
-export default App;
+const taksListListener = (index) =>{ 
+  setstate(prevstate =>{
+  const data = [...prevstate.toDoList]
+  data[index].completed = !data[index].completed;
+return{...prevstate, toDoList:data}
+})
+}
+
+const remainingTasks = () =>{
+ return  state.toDoList.filter(listItem =>!listItem.completed).length;     
+ }
+
+  return(
+          <div>
+            <h1>To-Do List</h1>
+            <div>
+              <input type="text" id="task" name="task" placeholder="Enter a task" onKeyDown={(event) => {if(event.keyCode === 13)
+                        document.getElementById('btnAdd').click()}}></input>
+              <button id="btnAdd" onClick={() => addTaskListener(document.getElementById('task').value)}>Click Me</button>
+              <p style={{color:"red"}}>{state.errorMessage}</p>
+            </div>
+            <div>
+              <ul>
+                {state.toDoList.map((taskItem,index)=>
+                  <li key={index} title ="Click item to mark completed" onClick = {()=>taksListListener(index)} className={taskItem.completed ? "list-item-completed":''}>{taskItem.taskName}</li>)}
+              </ul>
+                <p className="remaining-items">To-Do's Pending: {remainingTasks()}</p>
+            </div>
+          </div>  
+        )
+}
+
+export default Todo;
